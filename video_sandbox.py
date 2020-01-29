acd6d85b02e21a0ee0c15effd05c1f1fb0e2de1f
Magnus <magnus.haw@gmail.com>
2020-01-29 00:30:51 -0800

    ptf analysis

--- video_sandbox.py
+++ video_sandbox.py
@@ -3,11 +3,13 @@ import cv2 as cv
 from Frame import getModelROI
 
 fname = "video/AHF335Run001_EastView_1.mp4"
+fname = "video/trim.mov"
 cap = cv.VideoCapture(fname)
-
+counter=0
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    
     if ret==False:
         break
 
@@ -17,6 +19,8 @@ while(True):
         x,y,w,h, centerline = ret
         
     # Display the resulting frame
+##    cv.imwrite('frame%03d.png'%counter,frame)
+    counter +=1
     cv.imshow('frame',frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
