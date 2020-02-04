5a62e6649943d6545cea2b332a5f24050cbf21e8
Magnus <magnus.haw@gmail.com>
2020-02-04 00:06:26 -0800

    looking at convexhull now

--- cleanEdges.py
+++ cleanEdges.py
@@ -9,7 +9,7 @@ from scipy.interpolate import splev, splprep, interp1d
 folder = "video/"
 fname = "AHF335Run001_EastView_1.mp4"
 fname = "IHF360-005_EastView_3_HighSpeed.mp4"
-#fname = "IHF360-003_EastView_3_HighSpeed.mp4"
+fname = "IHF360-003_EastView_3_HighSpeed.mp4"
 ninterp = 200
 
 x0 = [885-60]
@@ -26,7 +26,7 @@ time=[]
 dfx = np.zeros((len(myc),ninterp+1))
 dfy = np.zeros((len(myc),ninterp+1))
 
-for i in range(0,len(myc),2):
+for i in range(0,len(myc),1):
     # clip bad corners
     c, ind = myc[i]
     start= c[0:50,0].argmin()
@@ -57,7 +57,9 @@ for i in range(0,len(myc),2):
     tck,u = splprep([x[okay],y[okay]],s=0)
     nu = np.linspace(0,1,ninterp)
     xi,yi = splev(nu,tck)
-
+    dl = np.sqrt(np.diff(x)**2 + np.diff(y)**2)
+    arclen = dl.sum()
+    
     # save data into matrix format
     dfx[i,1:]=xi
     dfy[i,1:]=yi
@@ -76,8 +78,8 @@ np.savetxt(folder+fname[0:-4] +'_Y.csv', dfy,delimiter=',')
 dy = np.array(highy)-np.array(lowy)
 plt.subplot(211)
 plt.title('ypos')
-#plt.plot(smooth(lowy),'r-')
-plt.plot(dy,'b-')
+plt.plot(smooth(lowy),'r-')
+plt.plot(smooth(highy),'b-')
 plt.subplot(212)
 plt.title('xpos')
 #plt.plot(smooth(lowx),'m-')
--- pp.py
+++ pp.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 from Functions import smooth
 from scipy.interpolate import splev, splprep, interp1d
 
-folder = "video/IHF360-003/"
+folder = "video/"
 fname = "IHF360-003_EastView_3_HighSpeed.mp4"
 
-folder= "video/IHF360-005/"
-fname = "IHF360-005_EastView_3_HighSpeed.mp4"
+##folder= "video/"
+##fname = "IHF360-005_EastView_3_HighSpeed.mp4"
 
 dfx = np.loadtxt(folder+fname[0:-4]+"_X.csv",delimiter=',')
 dfy = np.loadtxt(folder+fname[0:-4]+"_Y.csv",delimiter=',')
@@ -17,10 +17,26 @@ dfy = np.loadtxt(folder+fname[0:-4]+"_Y.csv",delimiter=',')
 time = dfx[:,0]
 cx = dfx[:,1:]
 cy = dfy[:,1:]
+clen = []
 
-#for row in range(0,len(cx),100):
+y75 = (cy[35,-1])*.75
+y50 = (cy[35,-1])*.5
+y00 = 0
 
-plt.plot(time,cx[:,250],'r-')
-plt.plot(time,cx[:,500],'g-')
-plt.plot(time,cx[:,750],'b-')
+c = np.zeros((200,1,2),dtype=np.int32)
+for row in range(35,2000,10):
+    c[:,0,0],c[:,0,1] = cy[row,:],cx[row,:]
+    hull = cv.convexHull(c)
+    y,x = hull[:,0,0],hull[:,0,1]
+    plt.plot(x,y,'-')
+    dl = np.sqrt(np.diff(cy[row,:])**2 + np.diff(cx[row,:])**2)
+    arclen = dl.sum()
+    print(time[row], cx[row,:].max())
+    clen.append(arclen)
+plt.show()
+
+plt.plot(clen)
+##plt.plot(time,cx[:,250],'r-')
+##plt.plot(time,cx[:,500],'g-')
+##plt.plot(time,cx[:,750],'b-')
 plt.show()
