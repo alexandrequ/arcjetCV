52afd1512a5af049546a4e33f6ca3beea24b9364
Magnus Haw <magnus.haw@nasa.gov>
2020-02-14 16:07:39 -0800

    massive reorg of folders

--- classes/PostProcess.py
+++ classes/PostProcess.py
@@ -7,28 +7,18 @@ from Calibrate import splitfn
 from scipy.interpolate import splev, splprep, interp1d
 from glob import glob
 
-folder = "video/IHF338/"
+    
+def processModelProps(myc,rnorms, minArea=7000, sample_radius=1.,fps=1.,
+                      fitindex=4,skip=4,
+                      PLOTXY=False,PLOTTIME=False,VERBOSE=False):
+    """
+    Processes output of getModelProps to extract axial (horizontal) position,
+    of 
 
-mask = folder + 'IHF338Run004_EastView_3_edges.pkl'  # default
-paths = glob(mask)
-
-### Units
-rnorms = [-.75,-.5,0,.5,0.75]
-rnorms = [0]
-labels = ['75% radius','50% radius','Apex','75% radius','50% radius']
-fps = 30
-minArea = 1200
-sample_radius = 1 #inches
-skip=1
-fitindex = 355
-PLOTXY=True;PLOTTIME=True;VERBOSE=True
-
-for path in paths:
-    pth, name, ext = splitfn(path)
-    fname = name+ext;print("### "+ name)
-
-    ### Load pickle file
-    fin = open(path,'rb');myc = np.array(pickle.load(fin));fin.close()
+    :param contour: opencv contour, shape(n,1,n)
+    :param ninterp: integer, number of interpolation points
+    :returns: output, interpolated contour positions
+    """
 
     ### Parse file
     t0,tf = myc[0,0],myc[-1,0]
@@ -40,15 +30,15 @@ for path in paths:
     flagList = myc[skip:-skip,5]
 
     R_px = dy.max()/2.
-
+    
     ### Filter by area,time
     finite_area = area > minArea
     ddt_area = np.append(np.zeros(1),abs(np.diff(area)) < 1200)
-
+    
     goodinds = np.nonzero(finite_area*ddt_area)[0]
-
+    
     ### Loop through pickle file
-    recess_pos =[]
+    recess_pos,t =[],[]
     for ind in goodinds:
         c = cntrs[ind]
         con = cv.convexHull(c)
@@ -58,14 +48,19 @@ for path in paths:
 
         # interpolate desired radial positions
         f = interp1d(y[2:-1], x[2:-1], kind='cubic')
-        xi = f(np.array(rnorms)*R_px)
+        try:
+            xi = f(np.array(rnorms)*R_px)
+            recess_pos.append(xi)
+            t.append(time[ind])
+        except:
+            xi = np.array(rnorms)*np.nan
         recess_pos.append(xi)
         
         if PLOTXY:
-            plt.plot(y*sample_radius/R_px,x*sample_radius/R_px,'o-')            
+            plt.plot(y*sample_radius/R_px,x*sample_radius/R_px,'-')            
 
     ### Cast list to array
-    rp = np.array(recess_pos)
+    rp,t = np.array(recess_pos),np.array(t)
     t = time[goodinds]
     sec = (t-t0)/fps
     xpos = rp*sample_radius/R_px
@@ -80,18 +75,44 @@ for path in paths:
             ys = rnorms[i]*R_px*np.ones(len(rp))
             plt.plot(ys*sample_radius/R_px,rp[:,i]*sample_radius/R_px,'x')
         plt.show()
+
     if PLOTTIME:
         err= 2*np.ones(len(sec))*sample_radius/R_px
         inds = np.arange(0,len(sec))
         err[inds>fitindex] /= 10000.
         for i in range(0,len(rnorms)):
-            plt.plot(sec,xpos[:,i],'o',label="%f"%rnorms[i])
+            plt.plot((t-t0)/fps,rp[:,i]*sample_radius/R_px,'-',label="%f"%rnorms[i])
             coeff,cov = np.polyfit(sec, xpos[:,i], 1,cov=True,w=1/err)
             dm = np.sqrt(cov[0,0])
-            #plt.plot(sec,sec*coeff[0]+coeff[1],'-')
+            plt.plot(sec,sec*coeff[0]+coeff[1],'-')
             dsec = (tf-t0)/fps
             print("slope: %f +- %f, %f R"%(coeff[0],dm*6,rnorms[i]))
             print("recession: %f +- %f, %f R"%(dsec*coeff[0],2*sample_radius/R_px,rnorms[i]))
-        plt.legend(loc=0)
+            plt.legend(loc=0)
         plt.show()
 
+    return sec,xpos
+
+if __name__ == "__main__":
+    folder = "video/IHF338/"
+
+    mask = folder + 'IHF338Run004_WestView_?_edges.pkl'  # default
+    paths = glob(mask)
+
+    ### PostProcess inputs
+    rnorms = [0]
+    fps = 30
+    minArea = 7000
+    sample_radius = 4 #inches
+    skip = 4
+    fitindex = 150
+    
+    for path in paths:
+        pth, name, ext = splitfn(path)
+        fname = name+ext;print("### "+ name)
+
+        ### Load pickle file
+        fin = open(path,'rb');myc = np.array(pickle.load(fin));fin.close()
+        t,rp = processModelProps(myc,rnorms,minArea=minArea,fps=fps,
+                                 sample_radius=4.,fitindex=fitindex,
+                                PLOTXY=True,PLOTTIME=True,VERBOSE=True)
--- postProcess_sandbox.py
+++ postProcess_sandbox.py
@@ -7,18 +7,28 @@ from Calibrate import splitfn
 from scipy.interpolate import splev, splprep, interp1d
 from glob import glob
 
-    
-def processModelProps(myc,rnorms, minArea=7000, sample_radius=1.,fps=1.,
-                      fitindex=4,skip=4,
-                      PLOTXY=False,PLOTTIME=False,VERBOSE=False):
-    """
-    Processes output of getModelProps to extract axial (horizontal) position,
-    of 
+folder = "video/IHF338/"
 
-    :param contour: opencv contour, shape(n,1,n)
-    :param ninterp: integer, number of interpolation points
-    :returns: output, interpolated contour positions
-    """
+mask = folder + 'IHF338Run005_WestView_3_edges.pkl'  # default
+paths = glob(mask)
+
+### Units
+rnorms = [-.75,-.5,0,.5,0.75]
+#rnorms = [0]
+labels = ['75% radius','50% radius','Apex','75% radius','50% radius']
+fps = 30
+minArea = 1200
+sample_radius = 2 #inches
+skip=1
+fitindex = 355
+PLOTXY=True;PLOTTIME=True;VERBOSE=True
+
+for path in paths:
+    pth, name, ext = splitfn(path)
+    fname = name+ext;print("### "+ name)
+
+    ### Load pickle file
+    fin = open(path,'rb');myc = np.array(pickle.load(fin));fin.close()
 
     ### Parse file
     t0,tf = myc[0,0],myc[-1,0]
@@ -30,15 +40,15 @@ def processModelProps(myc,rnorms, minArea=7000, sample_radius=1.,fps=1.,
     flagList = myc[skip:-skip,5]
 
     R_px = dy.max()/2.
-    
+
     ### Filter by area,time
     finite_area = area > minArea
     ddt_area = np.append(np.zeros(1),abs(np.diff(area)) < 1200)
-    
+
     goodinds = np.nonzero(finite_area*ddt_area)[0]
-    
+
     ### Loop through pickle file
-    recess_pos =[]
+    recess_pos,t =[],[]
     for ind in goodinds:
         c = cntrs[ind]
         con = cv.convexHull(c)
@@ -50,16 +60,17 @@ def processModelProps(myc,rnorms, minArea=7000, sample_radius=1.,fps=1.,
         f = interp1d(y[2:-1], x[2:-1], kind='cubic')
         try:
             xi = f(np.array(rnorms)*R_px)
+            recess_pos.append(xi)
+            t.append(time[ind])
         except:
             xi = np.array(rnorms)*np.nan
-        recess_pos.append(xi)
         
+
         if PLOTXY:
-            plt.plot(y*sample_radius/R_px,x*sample_radius/R_px,'o-')            
+            plt.plot(y*sample_radius/R_px,x*sample_radius/R_px,'-')            
 
     ### Cast list to array
-    rp = np.array(recess_pos)
-    t = time[goodinds]
+    rp,t = np.array(recess_pos),np.array(t)
     sec = (t-t0)/fps
     xpos = rp*sample_radius/R_px
 
@@ -73,44 +84,18 @@ def processModelProps(myc,rnorms, minArea=7000, sample_radius=1.,fps=1.,
             ys = rnorms[i]*R_px*np.ones(len(rp))
             plt.plot(ys*sample_radius/R_px,rp[:,i]*sample_radius/R_px,'x')
         plt.show()
-
     if PLOTTIME:
         err= 2*np.ones(len(sec))*sample_radius/R_px
         inds = np.arange(0,len(sec))
         err[inds>fitindex] /= 10000.
         for i in range(0,len(rnorms)):
-            plt.plot((t-t0)/fps,rp[:,i]*sample_radius/R_px,'-',label="%f"%rnorms[i])
+            plt.plot(sec,xpos[:,i],'o',label="%f"%rnorms[i])
             coeff,cov = np.polyfit(sec, xpos[:,i], 1,cov=True,w=1/err)
             dm = np.sqrt(cov[0,0])
-            plt.plot(sec,sec*coeff[0]+coeff[1],'-')
+            #plt.plot(sec,sec*coeff[0]+coeff[1],'-')
             dsec = (tf-t0)/fps
             print("slope: %f +- %f, %f R"%(coeff[0],dm*6,rnorms[i]))
             print("recession: %f +- %f, %f R"%(dsec*coeff[0],2*sample_radius/R_px,rnorms[i]))
-            plt.legend(loc=0)
+        plt.legend(loc=0)
         plt.show()
 
-    return sec,xpos
-
-if __name__ == "__main__":
-    folder = "video/IHF338/"
-
-    mask = folder + 'IHF338Run004_WestView_?_edges.pkl'  # default
-    paths = glob(mask)
-
-    ### PostProcess inputs
-    rnorms = [0]
-    fps = 30
-    minArea = 7000
-    sample_radius = 4 #inches
-    skip = 4
-    fitindex = 150
-    
-    for path in paths:
-        pth, name, ext = splitfn(path)
-        fname = name+ext;print("### "+ name)
-
-        ### Load pickle file
-        fin = open(path,'rb');myc = np.array(pickle.load(fin));fin.close()
-        t,rp = processModelProps(myc,rnorms,minArea=minArea,fps=fps,
-                                 sample_radius=4.,fitindex=fitindex,
-                                PLOTXY=True,PLOTTIME=True,VERBOSE=True)
