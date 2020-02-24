7af3979b24b0deae4d3f171a4b0945958de452d3
Magnus <magnus.haw@gmail.com>
2020-02-23 22:47:50 -0800

    forming mask from texture filter

--- postProcess_sandbox.py
+++ postProcess_sandbox.py
@@ -2,29 +2,31 @@ import numpy as np
 import cv2 as cv
 import pickle
 import matplotlib.pyplot as plt
-from classes.Functions import smooth,interpolateContour
+from classes.Functions import smooth,interpolateContour,getOrientation
 from classes.Calibrate import splitfn
 from scipy.interpolate import splev, splprep, interp1d
 from glob import glob
 
 folder = "video/IHF338/"
-mask = folder + 'IHF338Run004_WestView_?_edges.pkl'  # default
+mask = folder + 'IHF338Run004_WestView_3_edges.pkl'  # default
 
-folder = "video/HyMETS/"
-mask = folder + '*.pkl'  # default
+##folder = "video/HyMETS/"
+##mask = folder + 'PS12*.pkl'  # default
 
 paths = glob(mask)
 
 ### Units
 rnorms = [-.75,-.5,0,.5,0.75]
-#rnorms = [0]
+rnorms = [0]
 labels = ['75% radius','50% radius','Apex','75% radius','50% radius']
 fps = 30
 minArea = 1200
+flowDirection = 'right'
 sample_radius = 1 #inches
 skip=1
 fitindex = 0
 PLOTXY=True;PLOTTIME=True;VERBOSE=True
+WRITETOFILE = True
 
 for path in paths:
     pth, name, ext = splitfn(path)
@@ -55,20 +57,48 @@ for path in paths:
     for ind in goodinds:
         c = cntrs[ind]
         con = cv.convexHull(c)
+
+        ### Evaluate center/angle/direction of contour
+        th,cx,cyy,(x,y,w,h),fR = getOrientation(con)
+        if flowDirection == None:
+            if fR:
+                flowDirection = 'right'
+            else:
+                flowDirection = 'left'
+
         # offset vertical motion
         xi,yi = con[:,0,0],con[:,0,1]
-        x,y = xi,yi-cy[ind]
+        x,y = xi,yi-cyy#[ind]
+
+        # adjust starting location
+        if flowDirection == 'left':
+            imin = y.argmin()
+            y,x = np.roll(y,-imin),np.roll(x,-imin)
+
+            # remove non-function corner points
+            dyGreaterThanZero = np.append(np.zeros(1),np.diff(y) > 0)
+            okay = np.nonzero(dyGreaterThanZero)[0]
+        else:
+            # remove non-function corner points
+            dyLessThanZero = np.append(np.zeros(1),np.diff(y) < 0)
+            okay = np.nonzero(dyLessThanZero)[0]
 
         # interpolate desired radial positions
-        f = interp1d(y[2:-1], x[2:-1], kind='cubic')
+        f = interp1d(y[okay], x[okay], kind='cubic')
         try:
             xi = f(np.array(rnorms)*R_px)
             recess_pos.append(xi)
             t.append(time[ind])
         except:
             xi = np.array(rnorms)*np.nan
-        
 
+##        # interpolate full curve 
+##        try:
+##            yi = np.linspace(min(y[okay]),max(y[okay]),200)
+##            xis = f(yi)
+##        except:
+##            xis = yi*np.nan
+            
         if PLOTXY:
             plt.plot(y*sample_radius/R_px,x*sample_radius/R_px,'-')            
 
@@ -80,8 +110,8 @@ for path in paths:
     ### remove >2 pixel jumps
     diff = abs(np.diff(rp,axis=0))>2
     isDiffGT2 = np.vstack((diff[0,:]*0,diff))
-    rp[np.nonzero(isDiffGT2)[0]] = np.nan
-    
+    rp[np.nonzero(isDiffGT2)[0]] = np.nan    
+        
     if PLOTXY:
         for i in range(0,len(rnorms)):
             ys = rnorms[i]*R_px*np.ones(len(rp))
@@ -101,4 +131,8 @@ for path in paths:
             print("recession: %f +- %f, %f R"%(dsec*coeff[0],2*sample_radius/R_px,rnorms[i]))
         plt.legend(loc=0)
         plt.show()
+    if WRITETOFILE:
+        np.savetxt(name+'.csv',xpos,delimiter=',',header = str(rnorms)[1:-1])
+        np.savetxt(name+'_cy.csv',cy,delimiter=',',header = str(rnorms)[1:-1])
+        np.savetxt(name+'_time.csv',t-t0,delimiter=',',header = str(rnorms)[1:-1])
 
