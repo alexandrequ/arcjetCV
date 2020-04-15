490ee7ab2ee61b6d49098b26ed500c2162e82608
DAO TPM <alexandre.quintart@gmail.com>
2020-04-15 12:36:24 +0200

    add sandbox

---
+++ sandbox/CLAHE.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread('pika.png',0)
+
+# create a CLAHE object (Arguments are optional).
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+cl1 = clahe.apply(img)
+cv2.imwrite('clahe_2.jpg',cl1)
