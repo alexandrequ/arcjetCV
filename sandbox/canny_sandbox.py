ff58b302dacfac943888f94f3461ae7bd8df54c5
DAO TPM <alexandre.quintart@gmail.com>
2020-04-15 12:35:02 +0200

    add gradient edge detection and CLAHE

--- sandbox/canny_sandbox.py
+++ sandbox/canny_sandbox.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-img = cv.imread('snow-leopard.jpg',0)
-edges = cv.Canny(img,100,200)
+img = cv.imread('pika.png',0)
+edges = cv.Canny(img,20,5)
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
