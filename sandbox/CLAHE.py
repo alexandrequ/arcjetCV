1fc4bca1cc443265969a27dc422396256736922f
DAO TPM <alexandre.quintart@gmail.com>
2020-04-16 15:44:43 +0200

    gradient test

--- sandbox/CLAHE.py
+++ sandbox/CLAHE.py
@@ -2,9 +2,11 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread('pika.png',0)
-
-# create a CLAHE object (Arguments are optional).
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-cl1 = clahe.apply(img)
-cv2.imwrite('clahe_2.jpg',cl1)
+bgr = cv2.imread("pika.png")
+lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
+lab_planes = cv2.split(lab)
+clahe = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
+lab_planes[0] = clahe.apply(lab_planes[0])
+lab = cv2.merge(lab_planes)
+bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+cv2.imwrite('pika_clahe.png', bgr)
