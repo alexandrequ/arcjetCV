261b0f40bbe2c507aded0dd8dd946620a808abae
Alexandre <alexandre.quintart@gmail.com>
2020-04-30 19:39:00 +0200

    data augmentation

--- AI/shock-segmentation/pre-trained_test.py
+++ AI/shock-segmentation/pre-trained_test.py
@@ -4,17 +4,17 @@ from keras_segmentation.models.unet import vgg_unet
 
 dataset_path = os.getcwd() + "/dataset"
 
-model = vgg_unet(n_classes=51 ,  input_height=672, input_width=1216 )
+model = vgg_unet(n_classes=51 ,  input_height=128, input_width=128 )
 
 ################################################################
 
 
-model.train(
-    train_images =  dataset_path + "/images_train/",
-    train_annotations = dataset_path + "/masks_train/",
-    checkpoints_path = dataset_path + "/checkpoints/vgg_unet_1" , epochs=5
-)
-
+#model.train(
+#    train_images =  dataset_path + "/train_frames/",
+#    train_annotations = dataset_path + "/train_masks/",
+#    checkpoints_path = dataset_path + "/checkpoints/vgg_unet_1" , epochs=2
+#)
+model.load_weights("checkpoint")
 
 # load any of the 3 pretrained models
 
